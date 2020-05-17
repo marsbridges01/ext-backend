21d376f5cc879440008dc5d3049af9cfbe2ccbe1
marsbridges01 <marsbridges01@gmail.com>
2020-05-17 17:24:13 -0400

    0

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const http = require('http');
 const app = require('express')();
 const bodyParser = require('body-parser')
 
+const server = http.createServer(app);
+const port = 80;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = http.createServer(app);
-
 app.post('/hyou', (req, res) => {
   const vUrl = req.body.vUrl;
+
+  console.log(vUrl);
 
   if (shell.exec('youtube-dl --download-archive video.mp3 --format mp3 ' + vUrl).code !== 0) {
     console.log('done.');
   }
 });
 
-server.listen(3000, () => {
-  console.log('http server listening on port 1996');
+server.listen(port, () => {
+  console.log('http server listening on port ' + port);
 });
